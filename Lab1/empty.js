f446377473a2a083dc337f13aacc71d817f0f858
Rami <rami@Dell-XPS-15Z.(none)>
2014-11-17 16:13:21 +0200

    me1

--- Lab1/empty.js
+++ Lab1/empty.js
@@ -1,7 +1,18 @@
 $(document).ready(function(){
+	$(".animate").mouseenter(function() {
+		$(this).animate({
+    		margin: "1in",
+		}, 1500);
+	});
+	$(".animate").mouseleave(function() {
+		$(this).animate({
+    		margin: "0in",
+		}, 1500);
+	});
+
 	$("#senior").fadeIn(3000);
 	setTimeout(function(){
-		$(".guard").fadeIn();
+		$(".guard").fadeIn(5000);
 	},2000);
 	/*$("#senior").draggable();*/
 });
